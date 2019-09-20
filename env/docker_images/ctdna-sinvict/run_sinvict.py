b78a29ea3b8be03b7051f1935f17ba0936f16b17
erikwaskiewicz <32075672+erikwaskiewicz@users.noreply.github.com>
2019-09-20 12:46:27 +0100

    Reformat SiNVICT output into VCF

--- env/docker_images/ctdna-sinvict/run_sinvict.py
+++ env/docker_images/ctdna-sinvict/run_sinvict.py
@@ -2,6 +2,10 @@ import sys
 import subprocess
 import pandas as pd
 
+# =====================================================================
+# load in variables and run command line tools
+# =====================================================================
+
 input_bam = sys.argv[1]
 ref_fasta = sys.argv[2]
 output_file = sys.argv[3]
@@ -13,6 +17,7 @@ sample_name = '_'.join(output_file.split('_')[:-1])
 subprocess.call('mkdir /home/analysis_in', shell=True)
 
 # run bam-readcount, suppress warnings to 1 (otherwise will be one per read)
+# if fasta ref is not included, sinvict will call lots of Ns
 subprocess.call(
     f'''
     bam-readcount \
@@ -32,8 +37,13 @@ subprocess.call(
     shell=True
 )
 
+
+# =====================================================================
 # process sinvict output
-# merge all output, add column for which file variants came from
+# sinvict outputs 6 tsv files, one for each filter, we need to merge and convert to VCF
+# =====================================================================
+
+# make list of sinvict output files and the filter that was applied to each
 filter_list = (
     ('calls_level1.sinvict', '1-poisson_model'),
     ('calls_level2.sinvict', '2-min_read_depth'),
@@ -43,35 +53,65 @@ filter_list = (
     ('calls_level6.sinvict', '6-homopolymer_regions')
 )
 
+# label columns of sinvict output, 
+# capitals are ready to go into final vcf, lower case need processing first
+sinvict_out_cols = [
+    'chr', 'position', 'sample', 'REF', 'DP', 'ALT', 'SVR', 'percent', 
+    'mapped_plus', 'mapped_minus', 'ARP', 'SVT'
+]
+
 # make empty dataframe to store each iteration
-all_calls = pd.DataFrame(
-    columns=['#CHROM', 'POS', 'sample', 'REF', 'DP', 'ALT', 'allele_count', 'vaf', 'a', 'b', 'c', 'type', 'sinvict_filter']
-)
+all_calls = pd.DataFrame(columns=sinvict_out_cols.append('SVF'))
 
 # loop through each output file and add a column for filter type
-for item in filter_list:
-    filepath, filter_tag = item
-
+for filepath, filter_tag in filter_list:
     df = pd.read_csv(
         f'/home/{filepath}', 
         header=None,
         index_col=False,
         sep='\t', 
-        names=['#CHROM', 'POS', 'sample', 'REF', 'DP', 'ALT', 'allele_count', 'vaf', 'a', 'b', 'c', 'type']
+        names=sinvict_out_cols
     )
+    df['SVF'] = filter_tag
 
-    df['sinvict_filter'] = filter_tag
-
+    # add to main dataframe
     all_calls = pd.concat([all_calls, df], ignore_index=True)
 
 
-# TODO convert merged data into VCF
-# TODO ref and alt are wrong way round
-all_calls = all_calls.sort_values(by='POS')
-all_calls['a'] = all_calls['a'].str.strip('+:')
-all_calls['b'] = all_calls['b'].str.strip('-:')
-all_calls['#CHROM'] = all_calls['#CHROM'].map(str)
-all_calls['POS'] = all_calls['POS'].map(str)
+# =====================================================================
+# convert output to VCF
+# =====================================================================
+
+# make VCF header
+vcf_header = [
+    '##fileformat=VCFv4.2',
+    '##FILTER=<ID=PASS,Description="All filters passed">',
+    f'##reference={ref_fasta}',
+    '##ALT=<ID=*,Description="Represents allele(s) other than observed.">',
+    '##FORMAT=<ID=SVF,Number=.,Type=String,Description="sinvict filter">',
+    '##FORMAT=<ID=SVVAF,Number=.,Type=String,Description="sinvict vaf">',
+    '##FORMAT=<ID=DP,Number=.,Type=String,Description="depth">',
+    '##FORMAT=<ID=SVT,Number=.,Type=String,Description="sinvict type (germline/somatic)">',
+    '##FORMAT=<ID=SVR,Number=.,Type=String,Description="sinvict reads supporting">',
+    '##FORMAT=<ID=MPS,Number=.,Type=String,Description="mapped_plus_strand">',
+    '##FORMAT=<ID=MMR,Number=.,Type=String,Description="mapped_plus_strand">',
+    '##FORMAT=<ID=ARP,Number=.,Type=String,Description="average read pos">'
+]
+
+# write VCF header to file
+with open(output_file, 'w') as f:
+    f.writelines(line + '\n' for line in vcf_header)
+
+# process sinvict data before importing into VCF
+all_calls = all_calls.sort_values(by='position')
+
+all_calls['#CHROM'] = all_calls['chr'].map(str)
+all_calls['POS'] = all_calls['position'].map(str)
+
+all_calls['MPS'] = all_calls['mapped_plus'].str.strip('+:')
+all_calls['MMS'] = all_calls['mapped_minus'].str.strip('-:')
+all_calls['SVVAF'] = all_calls['percent'].div(10).round(3)
+
 all_calls['ID'] = '.'
 all_calls['QUAL'] = '.'
 all_calls['FILTER'] = '.'
@@ -79,42 +119,13 @@ all_calls['INFO'] = '.'
 
 # make format field
 all_calls['FORMAT'] = 'SVF:SVVAF:DP:SVT:SVR:MPS:MMS:ARP'
-all_calls[sample_name] = all_calls[[
-    'sinvict_filter', 'vaf', 'DP', 'type', 'allele_count', 'a', 'b', 'c'
-]].apply(lambda x: ':'.join(x.map(str)), axis=1)
 
-# write to file
- # TODO add VCF header and format field descriptions
-with open(output_file, 'w') as f:
-    f.writelines(
-f'''##fileformat=VCFv4.2
-##FILTER=<ID=PASS,Description="All filters passed">
-##reference={ref_fasta}
-##ALT=<ID=*,Description="Represents allele(s) other than observed.">
-##FORMAT=<ID=SVF,Type=String,Description="sinvict filter">
-''')
+all_calls[sample_name] = all_calls[
+    ['SVF', 'SVVAF', 'DP', 'SVT', 'SVR', 'MPS', 'MMS', 'ARP']
+].apply(lambda x: ':'.join(x.map(str)), axis=1)
 
-#all_calls = all_calls.astype(str)
-all_calls = all_calls[['#CHROM', 'POS', 'ID', 'REF', 'ALT', 'QUAL', 'FILTER', 'INFO', 'FORMAT', sample_name]]
+# write VCF body
+all_calls = all_calls[
+    ['#CHROM', 'POS', 'ID', 'REF', 'ALT', 'QUAL', 'FILTER', 'INFO', 'FORMAT', sample_name]
+]
 all_calls.to_csv(output_file, index=False, sep='\t', mode='a')
-
-
-'''
-format field descriptions
-SVF   - sinvict filter
-SVVAF - sinvict vaf
-SVT   - sinvict type
-SVR   - sinvict reads supporting
-MPS   - mapped_plus_strand
-MMS   - mapped_minus_strand
-ARP   - average read pos
-'''
-
-'''
-##fileformat=VCFv4.2
-##FILTER=<ID=PASS,Description="All filters passed">
-##reference=file://../test_data/Homo_sapiens_chr22_assembly19.fasta
-##contig=<ID=22,length=51304566>
-##ALT=<ID=*,Description="Represents allele(s) other than observed.">
-##FORMAT=<ID=SVF,Type=String,Description="sinvict filter">
-'''
